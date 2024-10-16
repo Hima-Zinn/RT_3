66f76e997f6d87618112a41107ff01730ca068ca
Hima <131072934+Hima-Zinn@users.noreply.github.com>
2024-10-16 06:03:00 +0000

    Fix: pages of home/help/about, routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  # Top Page
+  root "static_pages#home"
+  
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  root "hello#index"
 end
