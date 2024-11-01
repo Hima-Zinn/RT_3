21126de5512e42d08a7e51e6e4b954797b95a6db
Hima <131072934+Hima-Zinn@users.noreply.github.com>
2024-11-01 00:58:38 +0000

    Changed: page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # ページごとのタイトルを返す
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    #ページタイトルがなければ、base_titleのみ、あれば前に入力を追加
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
